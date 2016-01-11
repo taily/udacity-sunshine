506b5fb5a328db21eaf188fddf27b2b65239afbe
taily <taily_93@hotmail.com>
2016-01-11 00:00:47 -0500

    11/01/16

--- Sunshine/app/src/main/java/pe/com/tkongr/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/pe/com/tkongr/sunshine/app/MainActivityFragment.java
@@ -2,10 +2,19 @@ package pe.com.tkongr.sunshine.app;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -33,15 +42,67 @@ public class MainActivityFragment extends Fragment {
         };
 
         List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
-        
-       /* ArrayList<String> arrayList = new ArrayList<String>();
-        arrayList.add("Today - Sunny - 88/63");
-        arrayList.add("Tomorrow - Foggy - 70/46");
-        arrayList.add("Weds - Cloudy - 72/63");
-        arrayList.add("Thurds - Rainy - 64/51");
-        arrayList.add("Fri - Foggy - 70/46");
-        arrayList.add("Sat - Sunny - 76/68");*/
 
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+        ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
+        listView.setAdapter(arrayAdapter);
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String forecastJsonStr = null;
+
+        try {
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=La%20Molina,pe&mode=json&lang=es&units=metric&cnt=7&appid=1c8a031d4027f6b66bc39fdf1301fb67");
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            forecastJsonStr = buffer.toString();
+        } catch (IOException e) {
+            Log.e("PlaceholderFragment", "Error ", e);
+            return null;
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
 
         return view;
     }
